12fa3a9e24b60e01080a290e2ee755465bd3bad4
momozer <zerouali.mi@gmail.com>
2022-09-29 16:48:36 +0200

    style component share

--- src/app/share/share.component.ts
+++ src/app/share/share.component.ts
@@ -5,16 +5,19 @@ import { Component, OnInit} from '@angular/core';
   templateUrl: './share.component.html',
   styleUrls: ['./share.component.scss']
 })
+
 export class ShareComponent implements OnInit{
   titre !: string;
   description !: string;
   dateCreation !: Date;
   shares !: number;
+  imageURL !: string;
 
   ngOnInit(){
     this.titre = "Ariless";
     this.description = "Mon camarade de classe au collége !",
     this.dateCreation = new Date();
     this.shares = 6;
+    this.imageURL = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
     }
 }
