9f9f14e24ce079f7ae633d7e952fc12ae2e48422
Mohamed Zerouali <zerouali.mi@gmail.com>
2022-10-09 16:26:05 +0200

    modification mineure d'un element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceShare } from './model/face-share.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+  myShare !: FaceShare;
+  myOtherShare !: FaceShare;
+  myLastShare !: FaceShare;
+  ngOnInit() {
+    this.myShare = new FaceShare(
+      "Ariless",
+      "Mon camarade de classe au collége !",
+      new Date(),
+       0,
+      "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
+    );
+  }
 }
