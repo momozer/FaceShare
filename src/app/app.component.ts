62bdaba02ba50f25ce1a1941c1af2df666c717bb
Mohamed Zerouali <zerouali.mi@gmail.com>
2022-10-28 12:15:05 +0200

    suite test des observales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { Text } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { filter, interval, map, Observable } from 'rxjs';
+import { filter, interval, map, Observable, tap } from 'rxjs';
 
 
 @Component({
@@ -13,10 +14,12 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.interval$ = interval(1000).pipe(
       filter(value => value % 3 === 0),
-      map(value => value % 2 === 0 ? `je suis ${value} et je suis un nombre pair` : `Je suis ${value} et je suis un nombre impair`)
+      map(value => value % 2 === 0 ? `je suis ${value} et je suis un nombre pair` : `Je suis ${value} et je suis un nombre impair`),
+      tap(text => this.logger(text))
     );
-
-
-
+  }
+  logger(text:string){
+    console.log(`log: ${text}`);
+    
   }
 }
